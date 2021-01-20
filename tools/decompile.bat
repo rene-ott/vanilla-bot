5e925ccf7fbf1abcb3b07ea06f929d9661fe1c73
unknown <rene.ott6@gmail.com>
2021-01-21 01:06:39 +0200

    Remove unnecessary files.

--- tools/decompile.bat
+++
@@ -1,2 +0,0 @@
-java -cp java-decompiler.jar org.jetbrains.java.decompiler.main.decompiler.ConsoleDecompiler -hdc=0 -dgs=1 -rsy=1 -rbr=1 -lit=1 -nls=1 -mpm=60 C:\Users\Rene\Desktop\Repositories\vanilla-bot\vanilla-bot-rscvanilla\tools\client1.jar C:\Users\Rene\Desktop\Repositories\vanilla-bot\vanilla-bot-rscvanilla\tools\output
-pause
