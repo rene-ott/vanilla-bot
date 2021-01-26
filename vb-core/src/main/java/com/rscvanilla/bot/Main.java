f145c420cdac492a9c3cf02feb6939f30ee4f2a2
unknown <rene.ott6@gmail.com>
2021-01-26 20:59:37 +0200

    Add unfinished vanilla bot publisher task

--- vb-core/src/main/java/com/rscvanilla/bot/Main.java
+++ vb-core/src/main/java/com/rscvanilla/bot/Main.java
@@ -4,18 +4,34 @@ import com.google.inject.Guice;
 import com.rscvanilla.bot.bcel.CodeModifier;
 import com.rscvanilla.bot.infrastructure.BotException;
 import com.rscvanilla.bot.infrastructure.di.BotModule;
+import com.sun.tools.attach.AgentInitializationException;
+import com.sun.tools.attach.AgentLoadException;
+import com.sun.tools.attach.AttachNotSupportedException;
+import com.sun.tools.attach.VirtualMachine;
 import javassist.CannotCompileException;
 import javassist.NotFoundException;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
 import java.io.File;
+import java.io.IOException;
+import java.lang.management.ManagementFactory;
 
 public class Main {
 
     private static final Logger logger = LoggerFactory.getLogger(Main.class);
 
     public static void main(String [] args) {
+
+        try {
+            String nameOfRunningVM = ManagementFactory.getRuntimeMXBean().getName();
+            String pid = nameOfRunningVM.substring(0, nameOfRunningVM.indexOf('@'));
+            VirtualMachine vm = VirtualMachine.attach(pid);
+            vm.loadAgent(getUserDir() + File.separator + "lib" + File.separator + "vanilla-bot-agent.jar");
+            vm.detach();
+        } catch (IOException | AttachNotSupportedException | AgentLoadException | AgentInitializationException e) {
+            e.printStackTrace();
+        }
 
         try {
             Guice.createInjector(new BotModule(getCaptchaDirPath(), getScriptsDirectoryPath())).getInstance(VanillaBot.class).load();
